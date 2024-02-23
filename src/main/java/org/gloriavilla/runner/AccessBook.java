56615b3174ceb3f46bbfdefbe8d6e00eb3e7f447
Gloria Villa <hello.gloria@gmail.com>
2024-02-22 23:06:46 -0800

    DAO project created

---
+++ src/main/java/org/gloriavilla/runner/AccessBook.java
@@ -0,0 +1,36 @@
+package org.gloriavilla.runner;
+
+
+import org.gloriavilla.DAOinterface.BookDao;
+import org.gloriavilla.controller.BookDaoImpl;
+import org.gloriavilla.model.Books;
+
+import java.util.ArrayList;
+public class AccessBook {
+    public static void main(String[] args) {
+        System.out.println( "############# show list of all books ########## ");
+        BookDao bookDao = new BookDaoImpl();
+        for (Books b : bookDao.getAllBooks()) {
+            System.out.println("Book ISBN : " + b.getIsbn() +" and Book Name : " +
+                    b.getBookName() );
+        }
+
+        System.out.println("--------- After added/ inserted new book ----------");
+        Books b1 = new Books();
+        b1.setIsbn(4);
+        b1.setBookName("SQL Book");
+        bookDao.saveBook(b1);
+        for (Books b : bookDao.getAllBooks()) {
+            System.out.println("Book ISBN : " + b.getIsbn() +" and Book Name : " + b.getBookName() );
+        }
+
+        System.out.println("--------- After update book name ----------");
+        Books bookupdate = bookDao.getAllBooks().get(1);
+        bookupdate.setBookName("Algorithms Book");
+        for (Books b : bookDao.getAllBooks()) {
+            System.out.println("Book ISBN : " + b.getIsbn() +" and Book Name : " +
+                    b.getBookName() );
+        }
+    }
+}
+
